6158d41eaefa80b010d03ba67fe789ac11981a26
Cristian <cristiandepena@hotmail.com>
2019-12-17 15:51:20 -0400

    Refactoring controllers responses

--- controllers/product.js
+++ controllers/product.js
@@ -5,7 +5,12 @@ const Product = database.import('../models/product');
 const getProducts = (req, res) => {
   const products = Product.findAll().then(product => {
     console.log('All products: ', JSON.stringify(product, null, 4));
+    res.status(200).json({
+      product,
+      message:'Handling GET request'
+    });
   });
+  
 };
 
 module.exports = {
--- controllers/productCategory.js
+++ controllers/productCategory.js
@@ -5,6 +5,10 @@ const Category = database.import('../models/productCategory');
 const getCategories = (req, res) => {
   const categories = Category.findAll().then(category => {
     console.log('All product categories: ', JSON.stringify(category, null, 4));
+    res.status(200).json({
+      category,
+      message: 'Handling GET Request to /categories'
+    });
   });
 };
 
