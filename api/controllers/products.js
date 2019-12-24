03548887c6ed2d8b853e944cbbfcb2443f4b8dad
Cristian A. De Peña <dominicanxxx@gmail.com>
2019-12-24 00:53:48 -0400

    Refactoring

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,68 +1,67 @@
-const database = require('../../config/database');
-const Product = database.import('../models/products');
+const database = require("../../config/database");
+const Product = database.import("../models/products");
 
 // Get all products
 const getProducts = (req, res) => {
-  const products = Product.findAll().then(product => {
-    console.log('All products: ', JSON.stringify(product, null, 4));
-    res.status(200).json({
-      product,
-      message: 'Handling GET request to /products'
+    const products = Product.findAll().then(product => {
+        console.log("All products: ", JSON.stringify(product, null, 4));
+        res.status(200).json({
+            product,
+            message: "Handling GET request to /products"
+        });
     });
-  });
-
 };
 
 // Get product by id
 const getProductById = (req, res) => {
-  const id = req.body.id;
-  if (!id) {
-    res.status(401).json({
-      message: 'Invalid id'
-    });
-  } else {
-    const product = Product.findAll({ where: { ProductId: id } }).then(product => {
-      console.log('Product: ', JSON.stringify(product, null, 4));
-      res.status(200).json({
-        product,
-        message: 'Handling GET request to /products/ ' + id,
-      });
-    });
-  }
+    const id = req.body.id;
+    if (!id) {
+        res.status(401).json({
+            message: "Invalid id"
+        });
+    } else {
+        const product = Product.findAll({ where: { ProductId: id } }).then(
+            product => {
+                console.log("Product: ", JSON.stringify(product, null, 4));
+                res.status(200).json({
+                    product,
+                    message: "Handling GET request to /products/id " + id
+                });
+            }
+        );
+    }
 };
 
 // Create product
 const createProduct = (req, res) => {
-  const product = {
-    name: req.body.name
-  };
+    const product = {
+        name: req.body.name
+    };
 
-  res.status(201).json({
-    message: 'Handling POST request to /products',
-    createdProduct: product
-  });
+    res.status(201).json({
+        message: "Handling POST request to /products",
+        createdProduct: product
+    });
 };
-
 
 // Update product
 const updateProduct = (req, res) => {
-  res.status(200).json({
-    message: 'Handling PATCH request to /products'
-  });
+    res.status(200).json({
+        message: "Handling PATCH request to /products"
+    });
 };
 
 // Delete product
 const deleteProduct = (req, res) => {
-  res.status(200).json({
-    message: 'Handling DELETE request to /products'
-  });
+    res.status(200).json({
+        message: "Handling DELETE request to /products"
+    });
 };
 
-
 module.exports = {
-  getProducts,
-  getProductById,
-  createProduct,
-  updateProduct,
-  deleteProduct
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct
 };
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/products');
+const express = require("express");
+const {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct
+} = require("../controllers/products");
 
 const router = express.Router();
 
-router.get('/', getProducts);
-router.get('/:productId', getProductById);
-router.post('/', createProduct);
-router.patch('/:productId', updateProduct);
-router.delete('/:productId', deleteProduct);
-
+router.get("/", getProducts);
+router.get("/:productId", getProductById);
+router.post("/", createProduct);
+router.patch("/:productId", updateProduct);
+router.delete("/:productId", deleteProduct);
 
 module.exports = router;
