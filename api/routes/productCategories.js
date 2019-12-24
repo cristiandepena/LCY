e5a64f141ba601506e983582553472b2ea1df1da
Cristian A. De Peña <dominicanxxx@gmail.com>
2019-12-24 00:54:16 -0400

    Adding product categories routes

--- api/routes/productCategories.js
+++ api/routes/productCategories.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const { getCategories } = require('../controllers/productCategories');
+const {
+    getCategories,
+    getCategoryById,
+    createCategory,
+    updateCategory,
+    deleteCategory
+} = require('../controllers/productCategories');
 
 const router = express.Router();
 
 router.get('/', getCategories);
+router.get('/:categoryId', getCategoryById);
+router.post('/', createCategory);
+router.patch('/:categoryId', updateCategory);
+router.delete('/:categoryId', deleteCategory);
+
 
 module.exports = router;
