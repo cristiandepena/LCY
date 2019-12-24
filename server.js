d9605e3aa8f9b96e98e55d6252e21421fab99f05
Cristian A. De Peña <dominicanxxx@gmail.com>
2019-12-24 17:08:23 -0400

    Adding middleware to handle CORS errors

--- server.js
+++ server.js
@@ -39,6 +39,25 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+// Preventing CORS errors
+app.use((req, res, next) => {
+    // Allowing access to everyone to the API
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    };
+    next();
+});
+
 app.use('/products', productRoutes);
 app.use('/productCategories', productCategoriesRoutes);
 app.use('/users', userRoutes);
