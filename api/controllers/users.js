cfd6f5290c34f4266e675a6f60eb03b220bf4f02
Cristian A. De Peña <dominicanxxx@gmail.com>
2019-12-24 15:34:48 -0400

    Adding CRUD for users routes

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,18 +1,69 @@
 const database = require('../../config/database');
 const User = database.import('../models/users');
 
-// Get all products
+// Get all users
 const getUsers = (req, res) => {
-  const users = User.findAll().then(user => {
-    console.log('All Users: ', JSON.stringify(user, null, 4));
-    res.status(200).json({
-      users: user,
-      message:'Handling GET request for /users'
+    const users = User.findAll().then(user => {
+        console.log('All Users: ', JSON.stringify(user, null, 4));
+        res.status(200).json({
+            users: user,
+            message: 'Handling GET request for /users'
+        });
     });
-  });
-  
+
+};
+
+// Get user by id
+const getUserById = (req, res) => {
+    const id = req.body.id;
+    if (!id) {
+        res.status(401).json({
+            message: "Invalid id"
+        });
+    } else {
+        const user = User.findAll({ where: { UserId: id } }).then(
+            user => {
+                console.log("User: ", JSON.stringify(user, null, 4));
+                res.status(200).json({
+                    product,
+                    message: "Handling GET request to /users/id " + id
+                });
+            }
+        );
+    }
+};
+
+// Create user
+const createUser = (req, res) => {
+    const user = {
+        name: req.body.name
+    };
+
+    res.status(201).json({
+        message: "Handling POST request to /users",
+        createdUser: user
+    });
+};
+
+// Update user
+const updateUser = (req, res) => {
+    res.status(200).json({
+        message: "Handling PATCH request to /users"
+    });
+};
+
+// Delete product
+const deleteUser = (req, res) => {
+    res.status(200).json({
+        message: "Handling DELETE request to /users"
+    });
 };
 
 module.exports = {
-  getUsers
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+
 };
