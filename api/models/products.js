c0c49f903781ca346050a712f455e5bf4939aeac
Cristian <cristiandepena@hotmail.com>
2019-12-27 00:30:16 -0400

    Adding custom validation message

--- api/models/products.js
+++ api/models/products.js
@@ -3,45 +3,51 @@ const database = require('../../config/database');
 // const ProductCategory = database.import('./productCategories');
 
 module.exports = (sequelize, DataTypes) => {
-  const Product = sequelize.define('Products', {
-    ProductId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    Description: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    Stock: {
-      type: DataTypes.SMALLINT,
-      allowNull: false
-    },
-    Price: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    },
-    CategoryId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'ProductCategories',
-        key: 'CategoryId'
-      }
-    },
-    Active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
-    },
-    CreatedBy: {
-      type: DataTypes.SMALLINT,
-      allowNull: false
-    },
-  });
+    const Product = sequelize.define('Products', {
+        ProductId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        Description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [10, 200],
+                    msg: 'Description: requires a min of 10 to 200 characters'
+                }
+            }
+        },
+        Stock: {
+            type: DataTypes.SMALLINT,
+            allowNull: false
+        },
+        Price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        CategoryId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'ProductCategories',
+                key: 'CategoryId'
+            }
+        },
+        Active: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        CreatedBy: {
+            type: DataTypes.SMALLINT,
+            allowNull: false
+        },
+    });
 
-  // associations can be defined here
-  // Product.belongsTo(ProductCategory, {foreignKey: 'CategoryId'});
+    // associations can be defined here
+    // Product.belongsTo(ProductCategory, {foreignKey: 'CategoryId'});
 
 
-  return Product;
+    return Product;
 };
