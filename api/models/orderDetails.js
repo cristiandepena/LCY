1d97430bcba96ce682f6f7ba7180622492e5df2d
Cristian <cristiandepena@hotmail.com>
2019-12-28 18:57:50 -0400

    Refactoring associations

--- api/models/orderDetails.js
+++ api/models/orderDetails.js
@@ -1,6 +1,5 @@
 'use strict';
 const Sequelize = require('../../config/database');
-// const orders = Sequelize.import('./orders');
 const products = Sequelize.import('./products');
 
 
@@ -12,23 +11,6 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true,
       allowNull: false
     },
-    // OrderId: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    //   // references: {
-    //   //   model: 'Orders',
-    //   //   key: 'OrderId'
-    //   // }
-    // }
-    // ,
-    // ProductId: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    //   // references: {
-    //   //   model: 'Products',
-    //   //   key: 'ProductId'
-    //   // }
-    // },
     Quantity: {
       type: DataTypes.INTEGER,
       allowNull: false
@@ -39,9 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {timestamps: false});
 
-  // OrderDetails.belongsTo(orders, {
-  //   foreignKey: 'OrderId'
-  // });
+  // Associations
   OrderDetails.belongsTo(products, {
     foreignKey: 'ProductId'
   });
