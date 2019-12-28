d6c86bcaf5b14f25c11b5007629bb919f1100039
Cristian <cristiandepena@hotmail.com>
2019-12-27 22:35:41 -0400

    Logging associations

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const productRoutes = require('./api/routes/products');
 const productCategoriesRoutes = require('./api/routes/productCategories');
 const userRoutes = require('./api/routes/users');
 const orderRoutes = require('./api/routes/orders');
+const orderDetailsRoutes = require('./api/routes/orderDetails');
 
 const port = process.env.PORT || 8080;
 
@@ -18,12 +19,29 @@ database.authenticate()
   .catch(err => console.log(`Unable to connect: ${err}`));
 
 // REMOVE FORCE ON PROD
+
 database.sync({
   force: true,
   logging: console.log
 }).then((data) => {
+  // Associations
+  // data.models.Products.belongsTo(data.models.ProductCategories);
+  // data.models.ProductCategories.hasMany(data.models.Products);
+
+  // data.models.Orders.belongsTo(data.models.Users);  
+  // data.models.Users.hasMany(data.models.Orders);
+
+  // data.models.OrderDetails.belongsTo(data.models.Orders);
+  // data.models.Orders.hasMany(data.models.OrderDetails);
+
+  // data.models.Products.belongsTo(data.models.OrderDetails);
+  // data.models.OrderDetails.hasMany(data.models.Products);
+
   console.log('Database Synced successfully!');
-  console.log(data.models);
+  console.log('====ASSOCIATIONS====');
+  console.log(data.models.Products.associations);
+  console.log(data.models.Orders.associations);
+  console.log(data.models.OrderDetails.associations);
 });
 
 // Middlewares
@@ -55,6 +73,7 @@ app.use('/products', productRoutes);
 app.use('/productCategories', productCategoriesRoutes);
 app.use('/users', userRoutes);
 app.use('/orders', orderRoutes);
+app.use('/orderDetails', orderDetailsRoutes);
 
 // Custom Error Handling
 app.use((req, res, next) => {
