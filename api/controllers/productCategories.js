38b3180a5efed3e5374a22faa618ec890e58f05a
Cristian A. De Peña <dominicanxxx@gmail.com>
2019-12-31 13:30:00 -0400

    Refactoring complete CRUD

--- api/controllers/productCategories.js
+++ api/controllers/productCategories.js
@@ -3,18 +3,34 @@ const Category = database.import('../models/productCategories');
 
 // Get all categories
 const getCategories = (req, res) => {
-  const categories = Category.findAll().then(category => {
-    console.log('All product categories: ', JSON.stringify(category, null, 4));
-    res.status(200).json({
-      category,
-      message: 'Handling GET Request to /categories'
+  const categories = Category.findAll({
+    raw: true,
+    attributes: ['CategoryId', 'Description'],
+    where: {
+      Active: true
+    }
+  })
+    .then(category => {
+      const response = {
+        count: category.length,
+        categories: category.map(category => {
+          return {
+            id: category.CategoryId,
+            description: category.Description,
+            type: 'GET',
+            url: 'localhost:8080/productCategories/' + category.CategoryId
+          };
+        })
+      };
+      res.status(200).json({
+        Categories: response
+      });
     });
-  });
 };
 
 // Get category by id
 const getCategoryById = (req, res) => {
-  const id = req.body.id;
+  const id = req.params.categoryId;
   if (!id) {
     res.status(401).json({
       message: 'Invalid id'
@@ -25,11 +41,19 @@ const getCategoryById = (req, res) => {
         CategoryId: id
       }
     }).then(
-      product => {
-        console.log('Category: ', JSON.stringify(cat, null, 4));
+      category => {
+        const response = {
+          Categories: category.map(category => {
+            return {
+              id: category.CategoryId,
+              description: category.Description,
+              active: category.Active,
+              type: 'GET'
+            };
+          })
+        };
         res.status(200).json({
-          category: cat,
-          message: 'Handling GET request to /ProductCategories/ ' + id
+          response
         });
       }
     );
@@ -38,29 +62,76 @@ const getCategoryById = (req, res) => {
 
 // Create category
 const createCategory = (req, res, next) => {
-  const category = Category.create( {
+  const category = Category.create({
     Description: req.body.description,
     CreatedBy: req.body.createdBy
   }).then(row => {
+
     res.status(201).json({
       createdCategory: row.dataValues,
       message: 'Handling POST request to /ProductCategories',
+    });
+  }).catch(err => {
+    res.status(500).json({
+      error: err.message
     });
   });
 };
 
 // Update category
 const updateCategory = (req, res) => {
-  res.status(200).json({
-    message: 'Handling PATCH request to /ProductCategories'
-  });
+  const id = req.params.categoryId;
+  const description = req.body.description;
+
+  if (!id) {
+    res.status(500).json({
+      message: 'Invalid Id'
+    });
+  } else {
+    Category.update({
+      Description: description
+    }, {
+      where: {
+        CategoryId: id
+      }
+    })
+      .then(count => {
+        const response = {
+          message: `${count} Rows updated`,
+          type: 'PATCH'
+        };
+        res.status(200).json({
+          response
+        });
+      });
+
+  }
 };
 
 // Delete category
 const deleteCategory = (req, res) => {
-  res.status(200).json({
-    message: 'Handling DELETE request to /ProductCategories'
-  });
+  const id = req.params.categoryId;
+  if (!id) {
+    res.status(500).json({
+      message: 'Invalid Id'
+    });
+  } else {
+    Category.destroy({
+      where: {
+        CategoryId: id
+      }
+    })
+      .then(count => {
+        const response = {
+          message: `${count} Rows deleted`,
+          type: 'DELETE'
+        };
+        res.status(200).json({
+          response
+        });
+      });
+
+  }
 };
 
 module.exports = {
